d7d2bb6f7542c25c3d9c1f2bd289e03292e11e60
serybon <iblade@tut.by>
2024-10-05 20:20:42 +0300

    cycles-arrays second commit added ifCondition()

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,33 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        ifCondition();
+        System.out.println("End of Program");
+    }
+
+    public static void ifCondition() {
+        System.out.print("Please enter your score: ");
+        int userScore = new Scanner(System.in).nextInt();
+
+        while (userScore < 0 || userScore > 100) {
+            System.out.println("You entered incorrect data. Try again");
+            System.out.print("Please enter your score: ");
+            userScore = new Scanner(System.in).nextInt();
+        }
+        if (userScore >= 90) {
+            System.out.println("Your grade is A!");
+        } else if (userScore >= 80) {
+            System.out.println("Your grade is B!");
+        } else if (userScore >= 70) {
+            System.out.println("Your grade is C!");
+        } else if (userScore >= 60) {
+            System.out.println("Your grade is D!");
+        } else if (userScore >= 50) {
+            System.out.println("Your grade is E!");
+        } else {
+            System.out.println("Your grade is F!");
+        }
     }
 }
