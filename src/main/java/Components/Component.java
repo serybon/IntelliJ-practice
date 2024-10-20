3738b33cff144608de6712c9d7e7f2f33428b4b0
serybon <iblade@tut.by>
2024-10-20 23:41:10 +0300

    Maven installed

---
+++ src/main/java/Components/Component.java
@@ -0,0 +1,19 @@
+package Components;
+
+public class Component {
+    protected String vendor;
+    protected String model;
+    protected double price;
+
+    public String getVendor() {
+        return vendor;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+}
